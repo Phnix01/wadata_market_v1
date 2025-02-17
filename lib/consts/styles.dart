f17c9162fe95829e0115cf68e6bf5cbd657069be
Omar Farouk <fomar9235@gmail.com>
2025-02-17 15:17:37 +0100

    Add CartWidget and enhance cart screen functionality with shimmer effect

--- lib/consts/styles.dart
+++ lib/consts/styles.dart
@@ -4,11 +4,13 @@ import 'package:smart_shop_v1/consts/app_colors.dart';
 class Styles {
   static ThemeData themeData({required bool isDark, required BuildContext}) {
     return ThemeData(
-      scaffoldBackgroundColor:
-          isDark ? AppColors.darkScaffoldColor : AppColors.lightScaffoldColor,
-      cardColor:
-          isDark ? Color.fromARGB(255, 13, 6, 37) : AppColors.lightCardColor,
-      brightness: isDark ? Brightness.dark : Brightness.light,
-    );
+        scaffoldBackgroundColor:
+            isDark ? AppColors.darkScaffoldColor : AppColors.lightScaffoldColor,
+        cardColor:
+            isDark ? Color.fromARGB(255, 13, 6, 37) : AppColors.lightCardColor,
+        brightness: isDark ? Brightness.dark : Brightness.light,
+        appBarTheme: AppBarTheme(
+            iconTheme:
+                IconThemeData(color: isDark ? Colors.white : Colors.black)));
   }
 }
--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_shop_v1/screens/cart/cart_widget.dart';
 import 'package:smart_shop_v1/screens/cart/empty_card_widget.dart';
 import 'package:smart_shop_v1/services/assets_manager.dart';
 import 'package:smart_shop_v1/widgets/subtitle_text.dart';
@@ -9,6 +10,14 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: EmptyCardWidget());
+    bool _isEmpty = false;
+    return _isEmpty
+        ? Scaffold(body: EmptyCardWidget())
+        : Scaffold(
+            body: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return CartWidget();
+                }));
   }
 }
