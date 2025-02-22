812a94fed0f54ab78e0dbedf61f5b34117d1a787
Omar Farouk <fomar9235@gmail.com>
2025-02-22 16:49:37 +0100

    Implement search screen with text input and clear functionality

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:smart_shop_v1/services/assets_manager.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  late TextEditingController _searcherTextController;
+  @override
+  void initState() {
+    super.initState();
+    _searcherTextController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _searcherTextController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(
+            AssetsManager.shoppingCart,
+          ),
+        ),
+        title: Text(" Rechercher un produit"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(9.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                hintText: "nom du produit",
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.blue,
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _searcherTextController.clear();
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.clear,
+                    color: Colors.red,
+                  ),
+                ),
+                contentPadding: const EdgeInsets.all(20),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
