740de527e59c161310e9e6b06ccd324bc0ec6281
Omar Farouk <fomar9235@gmail.com>
2025-02-23 15:07:36 +0100

    Add CategoriesModel class and create category_rounded_widget file

---
+++ lib/widgets/products/category_rounded_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryRoundedWidget extends StatelessWidget {
+  const CategoryRoundedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
