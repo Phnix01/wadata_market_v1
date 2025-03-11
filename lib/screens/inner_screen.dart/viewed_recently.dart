6b697aca2198e97a98ef044822600ef174aa3847
Omar Farouk <fomar9235@gmail.com>
2025-03-11 12:21:38 +0100

    Add ViewedRecentlyScreen with dynamic grid layout for recently viewed products

---
+++ lib/screens/inner_screen.dart/viewed_recently.dart
@@ -0,0 +1,40 @@
+import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
+import 'package:flutter/material.dart';
+import 'package:smart_shop_v1/widgets/products/product_widget.dart';
+
+class ViewedRecentlyScreen extends StatelessWidget {
+  static const routName = "/ViewedRecentlyScreen";
+
+  const ViewedRecentlyScreen({super.key});
+  final bool isEmpty = false;
+  @override
+  Widget build(BuildContext context) {
+    return isEmpty
+        ? Scaffold(
+            body: Center(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: Text("historique de navigation "),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.delete_forever_rounded,
+                      color: Colors.red,
+                    ))
+              ],
+            ),
+            body: DynamicHeightGridView(
+              mainAxisSpacing: 12,
+              crossAxisSpacing: 12,
+              builder: (context, index) {
+                return const ProductWidget();
+              },
+              itemCount: 200,
+              crossAxisCount: 2,
+            ),
+          );
+  }
+}
