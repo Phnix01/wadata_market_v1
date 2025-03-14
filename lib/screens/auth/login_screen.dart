6e2278a20c0345abe1cd7ce975cfcb035d91565c
Omar Farouk <fomar9235@gmail.com>
2025-03-14 15:22:04 +0100

    add _emailFocusNode et _passwordFocusNode, design the element accross the form

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_shop_v1/widgets/app_text_widget.dart';
+import 'package:smart_shop_v1/widgets/title_text_widget.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,6 +13,20 @@ class _LoginScreenState extends State<LoginScreen> {
   late final TextEditingController _emailController;
   late final TextEditingController _passwordController;
 
+  late final FocusNode _emailFocus;
+  late final FocusNode _passwordFocusNode;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    _emailFocus = FocusNode();
+    _passwordFocusNode = FocusNode();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -23,7 +38,25 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: EdgeInsets.all(8.0),
           child: SingleChildScrollView(
               child: Column(
-            children: [const AppNameTextWidget()],
+            children: [
+              const SizedBox(
+                height: 60,
+              ),
+              const AppNameTextWidget(),
+              const SizedBox(
+                height: 15,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: TitleTextWidget(label: "Bon retour !! 😊"),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Form(
+                child: Column(),
+              )
+            ],
           )),
         ),
       ),
