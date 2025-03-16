44c83196784ce6314162cc96bc17858c90ab41d9
Omar Farouk <fomar9235@gmail.com>
2025-03-17 00:06:32 +0100

    Add error and warning dialog functionality to MyAppFunctions

---
+++ lib/services/my_app_functions.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:smart_shop_v1/services/assets_manager.dart';
+import 'package:smart_shop_v1/widgets/subtitle_text.dart';
+
+class MyAppFunctions {
+  static Future<void> showErrorOrWarningDialog({
+    required BuildContext context,
+    required String subtitle,
+    bool isError = true,
+    required Function fct,
+  }) async {
+    await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0)),
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  isError ? AssetsManager.error : AssetsManager.warning,
+                  height: 60,
+                  width: 60,
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SubtitleTextWidget(
+                  label: subtitle,
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Visibility(
+                      visible: !isError,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const SubtitleTextWidget(
+                          label: "Cancel",
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const SubtitleTextWidget(
+                        label: "OK",
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        });
+  }
+}
