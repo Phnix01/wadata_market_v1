f1a27fb98759901ed8240b0ebb6c27c3e629d766
Omar Farouk <fomar9235@gmail.com>
2025-03-29 03:53:24 +0100

    Refactor searchQuery method to accept a passed list for filtering products

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -26,8 +26,9 @@ class ProductsProvider extends ChangeNotifier {
     return categoryList;
   }
 
-  List<ProductModel> searchQuery({required String searchText}) {
-    List<ProductModel> searchList = products
+  List<ProductModel> searchQuery(
+      {required String searchText, required List<ProductModel> passedList}) {
+    List<ProductModel> searchList = passedList
         .where(
           (element) => element.productTitle
               .toLowerCase()
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -90,7 +90,8 @@ class _SearchScreenState extends State<SearchScreen> {
               onSubmitted: (value) {
                 setState(() {
                   productListSearch = productsProvider.searchQuery(
-                      searchText: _searcherTextController.text);
+                      searchText: _searcherTextController.text,
+                      passedList: productList);
                 });
               },
             ),
