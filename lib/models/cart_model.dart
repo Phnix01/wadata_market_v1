4ffe976db26f40dd9551018fa6b878e6316c3873
Omar Farouk <fomar9235@gmail.com>
2025-03-29 11:33:05 +0100

    Add CartModel class to manage cart items with product details

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,8 @@
+class CartModel {
+  final String CartId;
+  final String productId;
+  final String quantity;
+
+  CartModel(
+      {required this.CartId, required this.productId, required this.quantity});
+}
