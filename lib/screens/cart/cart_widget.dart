7b717c1cce5b29e70d6c7a7208a62572394c22d5
Omar Farouk <fomar9235@gmail.com>
2025-03-29 21:22:48 +0100

    Refactor CartWidget to conditionally render based on product presence and integrate ProductsProvider

--- lib/screens/cart/cart_widget.dart
+++ lib/screens/cart/cart_widget.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_shop_v1/models/cart_model.dart';
+import 'package:smart_shop_v1/providers/products_provider.dart';
 import 'package:smart_shop_v1/screens/cart/quantity_bottom_sheet.dart';
 import 'package:smart_shop_v1/widgets/subtitle_text.dart';
 import 'package:smart_shop_v1/widgets/title_text_widget.dart';
@@ -12,97 +15,103 @@ class CartWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cartModel = Provider.of<CartModel>(context);
+    final productsProvider = Provider.of<ProductsProvider>(context);
+    final getCurrentProduct =
+        productsProvider.findByProdId(cartModel.productId);
     Size size = MediaQuery.of(context).size;
-    return FittedBox(
-      child: IntrinsicWidth(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12.0),
-                child: FancyShimmerImage(
-                  imageUrl:
-                      'https://i.ibb.co/8r1Ny2n/20-Nike-Air-Force-1-07.png',
-                  height: size.height * 0.2,
-                  width: size.height * 0.2,
-                ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              IntrinsicWidth(
-                child: Column(
+    return getCurrentProduct == null
+        ? SizedBox.shrink()
+        : FittedBox(
+            child: IntrinsicWidth(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: size.width * 0.6,
-                          child: TitleTextWidget(
-                            label: "Title" * 15,
-                            maxLines: 2,
-                          ),
-                        ),
-                        Column(
-                          children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.clear,
-                                color: Colors.red,
-                              ),
-                            ),
-                            IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                IconlyLight.heart,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12.0),
+                      child: FancyShimmerImage(
+                        imageUrl:
+                            'https://i.ibb.co/8r1Ny2n/20-Nike-Air-Force-1-07.png',
+                        height: size.height * 0.2,
+                        width: size.height * 0.2,
+                      ),
                     ),
-                    Row(
-                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const SubtitleTextWidget(
-                          label: "16.00\$",
-                          color: Colors.blue,
-                        ),
-                        const Spacer(),
-                        OutlinedButton.icon(
-                          onPressed: () async {
-                            await showModalBottomSheet(
-                                backgroundColor:
-                                    Theme.of(context).scaffoldBackgroundColor,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(30),
-                                        topRight: Radius.circular(30))),
-                                context: context,
-                                builder: (context) {
-                                  return QuantityBottomSheet();
-                                });
-                          },
-                          icon: const Icon(IconlyLight.arrowDown2),
-                          label: const Text("Qty: 6"),
-                          style: OutlinedButton.styleFrom(
-                            side: const BorderSide(width: 1),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    IntrinsicWidth(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: size.width * 0.6,
+                                child: TitleTextWidget(
+                                  label: "Title" * 15,
+                                  maxLines: 2,
+                                ),
+                              ),
+                              Column(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.clear,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      IconlyLight.heart,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
+                          Row(
+                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const SubtitleTextWidget(
+                                label: "16.00\$",
+                                color: Colors.blue,
+                              ),
+                              const Spacer(),
+                              OutlinedButton.icon(
+                                onPressed: () async {
+                                  await showModalBottomSheet(
+                                      backgroundColor: Theme.of(context)
+                                          .scaffoldBackgroundColor,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(30),
+                                              topRight: Radius.circular(30))),
+                                      context: context,
+                                      builder: (context) {
+                                        return QuantityBottomSheet();
+                                      });
+                                },
+                                icon: const Icon(IconlyLight.arrowDown2),
+                                label: const Text("Qty: 6"),
+                                style: OutlinedButton.styleFrom(
+                                  side: const BorderSide(width: 1),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     )
                   ],
                 ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+              ),
+            ),
+          );
   }
 }
