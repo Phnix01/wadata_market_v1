f7886c9e42a461979e4dd89a5f7aac19f85105a7
Omar Farouk <fomar9235@gmail.com>
2025-04-03 02:29:13 +0100

    Add additional project files and prefab configuration for multiple architectures

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,26 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_shop_v1/providers/cart_provider.dart';
+import 'package:smart_shop_v1/screens/cart/bottom_checkout.dart';
 import 'package:smart_shop_v1/screens/cart/cart_widget.dart';
 import 'package:smart_shop_v1/screens/cart/empty_card_widget.dart';
 import 'package:smart_shop_v1/services/assets_manager.dart';
+import 'package:smart_shop_v1/widgets/empty_bag.dart';
 import 'package:smart_shop_v1/widgets/subtitle_text.dart';
 import 'package:smart_shop_v1/widgets/title_text_widget.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
-
+  final bool isEmpty = false;
   @override
   Widget build(BuildContext context) {
+    // final productsProvider = Provider.of<ProductsProvider>(context);
     final cartProvider = Provider.of<CartProvider>(context);
-    bool _isEmpty = false;
-    return _isEmpty
-        ? Scaffold(body: EmptyCardWidget())
+
+    return cartProvider.getCartItems.isEmpty
+        ? Scaffold(
+            body: EmptyBagWidget(
+              imagePath: AssetsManager.shoppingBasket,
+              title: "Votre Panier est vide",
+              subtitle:
+                  "Apparement Votre Panier est Vide, ajouter un produit dans votre Panier et Passez à l\'action",
+              buttonText: "Acheter maintenant",
+            ),
+          )
         : Scaffold(
-            body: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return CartWidget();
-                }));
+            bottomSheet: const CartBottomSheetWidget(),
+            appBar: AppBar(
+              leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  AssetsManager.shoppingCart,
+                ),
+              ),
+              title: TitleTextWidget(
+                  label: "Panier (${cartProvider.getCartItems.length})"),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete_forever_rounded,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: cartProvider.getCartItems.length,
+                      itemBuilder: (context, index) {
+                        return ChangeNotifierProvider.value(
+                            value: cartProvider.getCartItems.values
+                                .toList()[index],
+                            child: const CartWidget());
+                      }),
+                ),
+                const SizedBox(
+                  height: kBottomNavigationBarHeight + 10,
+                )
+              ],
+            ),
+          );
   }
 }
