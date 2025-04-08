e70890975c760b587eeb00b5178816533f2c57ef
Omar Farouk <fomar9235@gmail.com>
2025-04-08 20:55:28 +0100

    Add total calculation method in CartProvider to compute cart total based on product quantities

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_shop_v1/models/cart_model.dart';
+import 'package:smart_shop_v1/providers/products_provider.dart';
 import 'package:uuid/uuid.dart';
 
 class CartProvider extends ChangeNotifier {
@@ -18,5 +19,19 @@ class CartProvider extends ChangeNotifier {
 
   bool isProductInCart({required String productId}) {
     return _cartItems.containsKey(productId);
+  }
+
+  double getTotal({required ProductsProvider productsProvider}) {
+    double total = 0.0;
+    _cartItems.forEach((key, value) {
+      final getCurrentProduct = productsProvider.findByProdId(value.productId);
+      if (getCurrentProduct == null) {
+        total += 0.0;
+      } else {
+        total +=
+            double.parse(getCurrentProduct.productDescription) * value.quantity;
+      }
+    });
+    return total;
   }
 }
