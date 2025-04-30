45b4ae66afad0c60966218e1bd7fb2d91fb3accd
Omar Farouk <fomar9235@gmail.com>
2025-05-01 00:16:23 +0100

    Refactor WishlistScreen for improved readability and structure

--- lib/screens/inner_screen.dart/wishlist.dart
+++ lib/screens/inner_screen.dart/wishlist.dart
@@ -8,15 +8,49 @@ import 'package:smart_shop_v1/widgets/title_text_widget.dart';
 import 'package:smart_shop_v1/services/my_app_functions.dart';
 import 'package:smart_shop_v1/widgets/products/product_widget.dart';
 
+class WishlistScreen extends StatelessWidget {
+  static const String routName = "/WishlistScreen";
 
-class WishlistScreen extends StatelessWidget{
-  static routName = "/WishlistScreen";
+  @override
+  Widget build(BuildContext context) {
+    final wishlistProvider = Provider.of<WishlistProvider>(context);
 
-  bool isEmpty =
-  Widget build (BuildContext context){
-      final wishlistProvider = Provider.of<WishlistProvider>(context);
-      return wishlistProvider.getWishlists.isEmpty? Scaffold(body: EmptyBagWidget(imagePath: AssetsManager.bagWish, title: " Aucun Favoris", subtitle: " Vous attendez quoi pour aller liker les produits que vous aimez 😁", buttonText: "Allez consulter"),):Scaffold(appBar: AppBar(leading: Padding(padding: EdgeInsets.all(8.00),child:Image.asset(AssetsManager.shoppingCart) ,)title: TitleTextWidget(label: "Favoris (${wishlistProvider.getWishlists.length})"),),body: ,)
+    return wishlistProvider.getWishlists.isEmpty
+        ? Scaffold(
+            body: EmptyBagWidget(
+              imagePath: AssetsManager.bagWish,
+              title: "Aucun Favoris",
+              subtitle:
+                  "Vous attendez quoi pour aller liker les produits que vous aimez 😁",
+              buttonText: "Allez consulter",
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(AssetsManager.shoppingCart),
+              ),
+              title: TitleTextWidget(
+                label: "Favoris (${wishlistProvider.getWishlists.length})",
+              ),
+            ),
+            body: DynamicHeightGridView(
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              itemCount: wishlistProvider.getWishlists.length,
+              crossAxisCount: 2,
+              builder: (context, index) {
+                final productId = wishlistProvider.getWishlists.values
+                    .toList()[index]
+                    .productId;
+
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ProductWidget(productId: productId),
+                );
+              },
+            ),
+          );
   }
 }
-
-
